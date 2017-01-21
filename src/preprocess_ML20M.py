ff3260e774dff9c6d55fac156450c8cb4dfd0a00
Sumit Sidana <sumitsidana@sumits-macbook-pro.home>
2017-01-22 00:56:14 +0100

    adapt to ml100k

--- src/preprocess_ML20M.py
+++ src/preprocess_ML20M.py
@@ -26,7 +26,7 @@ sns.set(context="paper", font_scale=1.5, rc={"lines.linewidth": 2}, font='DejaVu
 
 # In[2]:
 
-DATA_DIR = '/data/sidana/nnmf_ranking/ml20m/'
+DATA_DIR = '/data/sidana/nnmf_ranking/ml100k/'
 
 
 # In[3]:
