c16253dbf2678cf6b66742683dbb65e4b0d19632
sumit <sumit.sidana@imag.fr>
2017-04-28 13:35:50 +0200

    add: preprocess for outbrain

--- src/preprocess_outbrain.py
+++ src/preprocess_outbrain.py
@@ -45,7 +45,7 @@ raw_data = pd.read_csv(os.path.join(DATA_DIR, 'ratings.csv'), header=0)
 # In[5]:
 #keep all ratings
 raw_all_data = raw_data[raw_data['rating'] > 0.0]
-
+raw_all_data = raw_all_data.sort_index(by=['timestamp'])
 # binarize the data (only keep ratings >= 4)
 raw_data = raw_data[raw_data['rating'] > 3.5]
 
@@ -303,9 +303,15 @@ test_all_index = np.where(raw_all_data['timestamp']==test_timestamp[0])[0][0]
 test_all_raw_data = raw_all_data[test_all_index:]
 test_all_raw_data = test_all_raw_data[test_all_raw_data['movieId'].isin(unique_sid)]
 test_all_raw_data = test_all_raw_data[test_all_raw_data['userId'].isin(unique_uid)]
+test_all_raw_data.to_csv(os.path.join(DATA_DIR, 'pro', 'test_all_raw.csv'), index=False)
 test_all_data = numerize(test_all_raw_data)
 test_all_data.to_csv(os.path.join(DATA_DIR, 'pro', 'test_all.csv'), index=False)
 
+
+train_all_raw_data = raw_all_data[0:test_all_index]
+train_all_raw_data = train_all_raw_data[train_all_raw_data['movieId'].isin(unique_sid)]
+train_all_raw_data = train_all_raw_data[train_all_raw_data['userId'].isin(unique_uid)]
+train_all_raw_data.to_csv(os.path.join(DATA_DIR, 'pro', 'train_all_raw.csv'), index=False)
 
 
 
